0060c8a2107005bfd2b2c22b15a82d006fdb780f
Andrew Jabasa <amjabasa@gmail.com>
2016-02-11 20:48:57 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
 	initializePage();
 })
 
+//string formatting
+String.prototype.format = String.prototype.f = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
+
+
 /*
  * Function that is called when the document is ready.
  */
@@ -26,7 +38,33 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	//var url1 = "/project/";
+	//var urlName = url1.concat(idNumber);
+	//var urlName = $(('/project/').attr(idNumber));
+	var urlName = '/project/' + idNumber;
+	var urlColor = '/palette';
+
 	console.log("User clicked on project " + idNumber);
+
+	$.get(urlName, addProject);
+	console.log("URL: " + urlName);
+
+	$.get(urlColor, randomizeColors);
+}
+
+function addProject(result){
+	console.log(result);
+	console.log("#project1 .details");
+	console.log("#project" + result['id'] + " .details");
+
+	var html = 
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] + 
+    '</small></p></a>' + '<p>' + result['summary'] + '</p>'; 
+	//var html = '<p>{0}</p><p><img {1} >{2}{3}</p'.format(result['title'], src="result['image']" .detailsImage, result['date'], result['summary']);
+	$("#project" + result['id'] + " .details").html(html);
+
 }
 
 /*
